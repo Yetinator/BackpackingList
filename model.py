bc98a76c6ed7156526ba5c4469a861da4f16cc8b
Brian <Helmethair@zoho.com>
2019-09-29 22:42:00 -0500

    stuff

--- model.py
+++ model.py
@@ -10,70 +10,35 @@ import backpackClass
 from configurations import *
 from hiddenSettings import *
 import dao.BackpackDao as BackpackDao
+import dao.ItemDao as ItemDao
 
 class Model():
 
     def __init__(self):
-        self.table = 'weightsSloppyImport'
-        self.allItems = []
+        # self.table = 'weightsSloppyImport'
         self.backpackDao = BackpackDao.BackpackDao()
+        self.itemDao = ItemDao.ItemDao()
+
+        #list of sql items
+        self.allItems = []
         #there is actually only one backpack unless the implementation gets weird
         self.backpack = []
+
+
         self.setItemList()
         self.setBackpackList()
 
 
     #Initializer
     def setItemList(self):
-        sqlQuery = "SELECT * from {table}".format(table=self.table)
-
-        connect = pql.connect(SQL_HOST, SQL_USER, SQL_PASSWORD, SQL_DATABASE)
-        cursor = connect.cursor()
-
-        cursor.execute(sqlQuery)
-        results = cursor.fetchall()
-
-        connect.close()
-
-        for record in results:
-            theItem = itemClass.Item(record[0], record[1], record[2], record[3])
-            self.allItems.append(theItem)
-
+        self.allItems = self.itemDao.findAll()
         return 0
 
     #Initializer
     def setBackpackList(self):
-        #later will call from sql and import appropriate backpacks or something
-        #userid, backpackid
-        # aBackpack = backpackClass.Backpack(1,1)
         aBackpack = self.backpackDao.findByUserIdAndBackpackId(0,0)
         self.backpack.append(aBackpack)
-
-    def getItemById(self, id):
-        sqlQuery = "SELECT * from {table} WHERE UNIQUE_ID = {ID}".format(table=self.table, ID = id)
-
-        connect = pql.connect(SQL_HOST, SQL_USER, SQL_PASSWORD, SQL_DATABASE)
-        cursor = connect.cursor()
-
-        cursor.execute(sqlQuery)
-        result = cursor.fetchone()
-
-        connect.close()
-        theItem = itemClass.Item(result[0], result[1], result[2], result[3])
-        return theItem
+        return 0
 
     def getItemList(self):
         return self.allItems
-
-
-    # def getItemInfo(self, item):
-    #     sqlQuery = "SELECT * from {table} WHERE UNIQUE_ID = '{item}'".format(table=self.table, item=item)
-    #
-    #     connect = pql.connect(SQL_HOST, SQL_USER, SQL_PASSWORD, SQL_DATABASE)
-    #     cursor = connect.cursor()
-    #
-    #     cursor.execute(sqlQuery)
-    #     data = cursor.fetchone()
-    #
-    #     connect.close()
-    #     return data
