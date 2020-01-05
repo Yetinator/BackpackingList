00138652689d1334830b7fa7abffe6bd028fbff8
Brian <Helmethair@zoho.com>
2020-01-05 11:51:49 -0600

    quick commit

--- dao/PopulateItem.py
+++ dao/PopulateItem.py
@@ -6,11 +6,26 @@
 import os
 import subprocess
 import csv
+import pymysql as pql
+from configurations import *
+from hiddenSettings import *
 
 
 def populateCoolSwag():
     #move data from csv to sql table COOL_SWAG
     print("hello")
+    inputs = "(U_ID, BRAND, NAME, WEIGHT_GRAMS, WHOLE_UNIT, CONTRIBUTER_ID, MODEL_YEAR, WEIGHT_OZ, PICTURE_URL, TIME_STAMP)"
+    values = "('0', 'Kelty', 'Trekker 6500', '2400', '1', '0', '1234567', '84.7', 'Default.jpg', )"
+    sql = "insert into COOL_SWAG {inputs}  VALUES {values};".format(inputs=inputs, values=values)
+
+    connect = pql.connect(SQL_HOST, SQL_USER, SQL_PASSWORD, SQL_DATABASE)
+    cursor = connect.cursor()
+
+    cursor.execute(sqlQuery)
+    # result = cursor.fetchone()
+
+    connect.close()
+
 
 
 populateCoolSwag()
